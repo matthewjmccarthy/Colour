c110c775c747c8f4a78b1c63093ae9e43c3c58f5
matthew <matthewjmccarthy2001@gmail.com>
2022-11-29 17:55:02 +0000

    Created failing test for three parameter Colour constructor

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -12,4 +12,13 @@ public class ColourTest {
         assertEquals(0.0f, colour.getGreen(), 0.0);
         assertEquals(0.5f, colour.getBlue(), 0.0);
     }
+
+    @Test
+    @DisplayName("Second test of the three parameter Colour constructor.")
+    public void test3ValueColourConstructor2() {
+        Colour colour = new Colour(0.0f, 0.5f, 1.0f);
+        assertEquals(0.0f, colour.getRed(), 0.0);
+        assertEquals(0.5f, colour.getGreen(), 0.0);
+        assertEquals(1.0f, colour.getBlue(), 0.0);
+    }
 }
