c8259accd3d005faba84ac91fc8e23106033d047
matthew <matthewjmccarthy2001@gmail.com>
2022-11-29 14:25:01 +0000

    Created initial and  simplest test on three parameter Colour constructor.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,2 +1,6 @@
 public class Colour {
+    public Colour(float red, float green, float blue) {}
+    public float getRed() { return 1.0f; }
+    public float getGreen() { return 0.0f; }
+    public float getBlue() { return 0.5f; }
 }
--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ColourTest {
+
+    @Test
+    @DisplayName("Test of the three parameter Colour constructor.")
+    public void test3ValueColourConstructor() {
+        Colour colour = new Colour(1.0f, 0.0f, 0.5f);
+        assertEquals(1.0f, colour.getRed(), 0.0);
+        assertEquals(0.0f, colour.getGreen(), 0.0);
+        assertEquals(0.5f, colour.getBlue(), 0.0);
+    }
 }
