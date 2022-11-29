96a4a0fc69423f7d244a7addf0ca6e5e7ed477c9
matthew <matthewjmccarthy2001@gmail.com>
2022-11-29 17:58:06 +0000

    Updated Colour class to pass 2nd test.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,6 +1,14 @@
 public class Colour {
-    public Colour(float red, float green, float blue) {}
-    public float getRed() { return 1.0f; }
-    public float getGreen() { return 0.0f; }
-    public float getBlue() { return 0.5f; }
+    float red_value;
+    float green_value;
+    float blue_value;
+
+    public Colour(float red, float green, float blue) {
+        this.red_value = red;
+        this.green_value = green;
+        this.blue_value = blue;
+    }
+    public float getRed() { return this.red_value; }
+    public float getGreen() { return this.green_value; }
+    public float getBlue() { return this.blue_value; }
 }
